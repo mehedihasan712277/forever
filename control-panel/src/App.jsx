b01efb86964c0d57ec8f272d3d2c58eaa1117332
mehedihasan712277 <mehedihasan712277@gmail.com>
2024-02-20 16:54:16 +0600

    rroute setup done

--- control-panel/src/App.jsx
+++ control-panel/src/App.jsx
@@ -1,19 +1,50 @@
 import React from 'react'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-//import Error from './Error'                    (import yourself)
-//import AuthProvider from './AuthProvider'      (import yourself)
+import MainContainer from './MainContainer'
+import Error from './pages/Error'
+import Orders from './pages/Orders'
+import AllProducts from './pages/AllProducts'
+import Add from './pages/Add'
+import Carts from './pages/Carts'
+import Banner from './pages/Banner'
+import Details from './pages/Details'
+import Update from './pages/Update'
 
 
 const allRoute = createBrowserRouter([
   {
     path: "/",
-    element: <></>,
+    element: <MainContainer></MainContainer>,
     errorElement: <Error></Error>,//This component must be imported
     children: [
       {
         path: "/",
-        element: <></>
+        element: <Orders></Orders>
       },
+      {
+        path: "/allproduct",
+        element: <AllProducts></AllProducts>
+      },
+      {
+        path: "/add",
+        element: <Add></Add>
+      },
+      {
+        path: "/carts",
+        element: <Carts></Carts>
+      },
+      {
+        path: "/banner",
+        element: <Banner></Banner>
+      },
+      {
+        path: "/details/:id",
+        element: <Details></Details>
+      },
+      {
+        path: "/update/:id",
+        element: <Update></Update>
+      }
     ]
   }
 ])
