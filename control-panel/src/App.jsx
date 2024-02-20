6f00aadb255be4b3e839247f1631c8f882b4206d
mehedihasan712277 <mehedihasan712277@gmail.com>
2024-02-20 16:45:35 +0600

    initial setup done

--- control-panel/src/App.jsx
+++ control-panel/src/App.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+//import Error from './Error'                    (import yourself)
+//import AuthProvider from './AuthProvider'      (import yourself)
+
+
+const allRoute = createBrowserRouter([
+  {
+    path: "/",
+    element: <></>,
+    errorElement: <Error></Error>,//This component must be imported
+    children: [
+      {
+        path: "/",
+        element: <></>
+      },
+    ]
+  }
+])
 
 const App = () => {
   return (
-    <div>App</div>
+    <AuthProvider>
+      <RouterProvider router={allRoute}></RouterProvider>
+    </AuthProvider>
   )
 }
 
