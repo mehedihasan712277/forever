f56dff9dfe06f5b68e8f14fc2200a3d8e4664dbc
mehedihasan712277 <mehedihasan712277@gmail.com>
2024-02-22 00:52:38 +0600

    loader added

--- control-panel/src/pages/Add.jsx
+++ control-panel/src/pages/Add.jsx
@@ -1,10 +1,13 @@
 import axios from 'axios';
-import React from 'react'
+import React, { useState } from 'react'
 import Swal from 'sweetalert2';
 
 const Add = () => {
+    const [loading, setLoading] = useState(false)
     const handleAddProduct = (event) => {
         event.preventDefault();
+        setLoading(true);
+
         const form = event.target;
         const productName = form.productName.value;
         const regularPrice = Number(form.regularPrice.value);
@@ -29,7 +32,6 @@ const Add = () => {
             sold,
             category
         }
-        console.log(info);
         axios.post(`https://server-forever.vercel.app/control`, info)
             .then(res => {
                 Swal.fire({
@@ -37,50 +39,57 @@ const Add = () => {
                     text: "Product added successfully",
                     icon: "success"
                 });
+                setLoading(false);
                 form.reset();
             })
     }
     return (
         <>
             <div>
+                {
+                    loading &&
+                    <div className='fixed top-0 left-0 w-full h-screen z-20 text-center' style={{ background: 'linear-gradient(to right, rgba(0,0,0,0.5), rgba(0,0,0,0.5))' }}>
+                        <span className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 bg-cyan-500 loading loading-infinity loading-lg"></span>
+                    </div>
+                }
                 <section>
-                    <div class="max-w-2xl px-4 py-8 mx-auto lg:py-16">
-                        <p class="mb-4 text-2xl text-center font-bold text-cyan-400">Add product</p>
+                    <div className="max-w-2xl px-4 py-8 mx-auto lg:py-16">
+                        <p className="mb-4 text-2xl text-center font-bold text-cyan-400">Add product</p>
                         <form onSubmit={handleAddProduct}>
-                            <div class="grid gap-4 mb-4 sm:grid-cols-2 sm:gap-6 sm:mb-5">
-                                <div class="sm:col-span-2">
-                                    <label for="name" class="block mb-2 text-sm font-medium text-gray-900">Product Name</label>
-                                    <input type="text" name="productName" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="Type product name" required />
+                            <div className="grid gap-4 mb-4 sm:grid-cols-2 sm:gap-6 sm:mb-5">
+                                <div className="sm:col-span-2">
+                                    <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900">Product Name</label>
+                                    <input type="text" name="productName" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="Type product name" required />
                                 </div>
                                 {/* -----------images-------------- */}
-                                <div class="w-full">
-                                    <label for="img1" class="block mb-2 text-sm font-medium text-gray-900">Image 1</label>
-                                    <input type="text" name="img1" id="img1" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="link" required />
+                                <div className="w-full">
+                                    <label htmlFor="img1" className="block mb-2 text-sm font-medium text-gray-900">Image 1</label>
+                                    <input type="text" name="img1" id="img1" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="link" required />
                                 </div>
-                                <div class="w-full">
-                                    <label for="img2" class="block mb-2 text-sm font-medium text-gray-900">Image 2</label>
-                                    <input type="text" name="img2" id="img2" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="link" required />
+                                <div className="w-full">
+                                    <label htmlFor="img2" className="block mb-2 text-sm font-medium text-gray-900">Image 2</label>
+                                    <input type="text" name="img2" id="img2" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="link" required />
                                 </div>
-                                <div class="w-full">
-                                    <label for="img3" class="block mb-2 text-sm font-medium text-gray-900">Image 3</label>
-                                    <input type="text" name="img3" id="img3" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="link" required />
+                                <div className="w-full">
+                                    <label htmlFor="img3" className="block mb-2 text-sm font-medium text-gray-900">Image 3</label>
+                                    <input type="text" name="img3" id="img3" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="link" required />
                                 </div>
-                                <div class="w-full">
-                                    <label for="img4" class="block mb-2 text-sm font-medium text-gray-900">Image 4</label>
-                                    <input type="text" name="img4" id="img4" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="link" required />
+                                <div className="w-full">
+                                    <label htmlFor="img4" className="block mb-2 text-sm font-medium text-gray-900">Image 4</label>
+                                    <input type="text" name="img4" id="img4" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="link" required />
                                 </div>
                                 {/* -------------------------------------- */}
-                                <div class="w-full">
-                                    <label for="brand" class="block mb-2 text-sm font-medium text-gray-900">Regular Price</label>
-                                    <input type="number" name="regularPrice" id="brand" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="120" required />
+                                <div className="w-full">
+                                    <label htmlFor="brand" className="block mb-2 text-sm font-medium text-gray-900">Regular Price</label>
+                                    <input type="number" name="regularPrice" id="brand" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="120" required />
                                 </div>
-                                <div class="w-full">
-                                    <label for="price" class="block mb-2 text-sm font-medium text-gray-900">Reduced Price</label>
-                                    <input type="number" name="reducedPrice" id="price" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="100" required />
+                                <div className="w-full">
+                                    <label htmlFor="price" className="block mb-2 text-sm font-medium text-gray-900">Reduced Price</label>
+                                    <input type="number" name="reducedPrice" id="price" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="100" required />
                                 </div>
                                 <div>
-                                    <label for="category" class="block mb-2 text-sm font-medium text-gray-900">Category</label>
-                                    <select id="category" name='category' class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5">
+                                    <label htmlFor="category" className="block mb-2 text-sm font-medium text-gray-900">Category</label>
+                                    <select id="category" name='category' className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5">
                                         <option value="nutrition">nutrition</option>
                                         <option value="bee">bee</option>
                                         <option value="weight">weight</option>
@@ -91,24 +100,22 @@ const Add = () => {
                                     </select>
                                 </div>
                                 <div>
-                                    <label for="item-weight" class="block mb-2 text-sm font-medium text-gray-900">Sold</label>
-                                    <input type="number" name="sold" id="item-weight" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="Ex. 12" required />
+                                    <label htmlFor="item-weight" className="block mb-2 text-sm font-medium text-gray-900">Sold</label>
+                                    <input type="number" name="sold" id="item-weight" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="Ex. 12" required />
                                 </div>
-                                <div class="sm:col-span-2">
-                                    <label for="description" class="block mb-2 text-sm font-medium text-gray-900">Description</label>
-                                    <textarea id="description" rows="8" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500" placeholder="Write a product description here..."></textarea>
+                                <div className="sm:col-span-2">
+                                    <label htmlFor="description" className="block mb-2 text-sm font-medium text-gray-900">Description</label>
+                                    <textarea id="description" rows="8" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500" placeholder="Write a product description here..."></textarea>
                                 </div>
                             </div>
-                            <div class="flex items-center justify-center">
-                                <button type="submit" class="text-cyan-500 inline-flex items-center hover:text-white border border-cyan-500 hover:bg-cyan-500 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+                            <div className="flex items-center justify-center">
+                                <button type="submit" className="text-cyan-500 inline-flex items-center hover:text-white border border-cyan-500 hover:bg-cyan-500 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
                                     Add
                                 </button>
                             </div>
                         </form>
                     </div>
                 </section>
-
-
             </div>
         </>
     )
--- control-panel/src/pages/AllProducts.jsx
+++ control-panel/src/pages/AllProducts.jsx
@@ -1,9 +1,112 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import { BsBackspaceReverseFill, BsPencilSquare } from "react-icons/bs";
+import Swal from 'sweetalert2';
 
 const AllProducts = () => {
+    const [data, setData] = useState(null);
+    const [refresh, setRefresh] = useState(false);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        axios.get(`https://server-forever.vercel.app/products`)
+            .then(res => {
+                setData(res.data);
+                setLoading(false);
+            })
+    }, [refresh])
+
+    if (loading) {
+        return <div className='py-40 w-full flex justify-center items-center'>
+            <span className="loading loading-spinner text-accent"></span>
+        </div>
+    }
+
+
+
+    const handleDeleteProduct = (id) => {
+
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axios.delete(`https://server-forever.vercel.app/control/${id}`)
+                    .then(res => {
+                        Swal.fire({
+                            title: "Deleted!",
+                            text: "The product has been deleted",
+                            icon: "success"
+                        });
+                        setLoading(true)
+                        setRefresh(!refresh);
+                    });
+            }
+        });
+    }
     return (
         <>
-            hi
+            <div className='text-[#374259]'>
+                {
+                    data.length ?
+                        <div className='flex flex-wrap gap-2 ml:gap-4 justify-center py-6'>
+                            {
+                                data.map(ele => {
+                                    return <div key={ele._id} className='bg-[#D2E9E9] p-2 ml:p-3 xl:p-4 rounded-md'>
+                                        <div className='w-32 mm:w-40 lg:w-44 xl:w-48 h-full flex flex-col justify-between'>
+                                            <div>
+                                                <div className='h-36 lg:h-40 xl:h-52 bg-white rounded-md'>
+                                                    <img src={ele.img1} alt="img" className='h-36 lg:h-40 xl:h-52 w-full rounded-md' />
+                                                </div>
+                                                <p className='text-xs md:text-sm lg:text-lg font-bold'>{ele.productName}</p>
+                                                <div className='flex flex-wrap gap-1 md:gap-2 xl:gap-1 text-xs xl:text-sm'>
+                                                    <p>Price(Tk): {ele.reducedPrice}</p>
+                                                    <del className='text-gray-500'>{ele.regularPrice}</del>
+                                                    <p className='text-red-700'>
+                                                        {
+                                                            Number.parseInt(((ele.regularPrice - ele.reducedPrice) / ele.reducedPrice) * 100)
+                                                        }
+                                                        %off
+                                                    </p>
+                                                </div>
+                                                <p>Sold: {ele.sold}</p>
+                                            </div>
+
+                                            <div className='flex justify-between gap-1'>
+                                                <button onClick={() => handleDeleteProduct(ele._id)} className=' btn btn-sm text-[#374259] bg-transparent border-none shadow-none'><BsBackspaceReverseFill className='text-xl' /></button>
+                                                <Link to={`/update/${ele._id}`} state={{
+                                                    productName: ele.productName,
+                                                    regularPrice: ele.regularPrice,
+                                                    reducedPrice: ele.reducedPrice,
+                                                    img1: ele.img1,
+                                                    img2: ele.img2,
+                                                    img3: ele.img3,
+                                                    img4: ele.img4,
+                                                    description: ele.description,
+                                                    sold: ele.sold,
+                                                    category: ele.category,
+                                                    id: ele._id
+                                                }}>
+                                                    <button className=' btn btn-sm text-[#374259] bg-transparent border-none shadow-none'><BsPencilSquare className='text-xl' /></button>
+                                                </Link>
+                                            </div>
+                                        </div>
+                                    </div>
+                                })
+                            }
+                        </div>
+                        :
+                        <div className='py-40 font-bold text-gray-700 text-center'>
+                            No product
+                        </div>
+                }
+            </div>
         </>
     )
 }
