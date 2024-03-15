b551e679293172d65748e2e2d981ebb50486edfe
mehedihasan712277 <mehedihasan712277@gmail.com>
2024-03-15 07:40:26 +0600

    deleted some code

--- client/src/page/principal/public/Home.jsx
+++ client/src/page/principal/public/Home.jsx
@@ -1,15 +1,11 @@
 import React from 'react'
 import Banner from './Banner';
-import CountdownTimer from './CountdownTimer';
-import Deals from './Deals';
 import Products from './Products';
 
 const Home = () => {
     return (
         <>
             <Banner></Banner>
-            {/* <CountdownTimer></CountdownTimer>
-            <Deals></Deals> */}
             <Products></Products>
         </>
     )
